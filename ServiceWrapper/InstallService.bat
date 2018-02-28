ec1b19eaf83df988e8c661f07725d573912220ef
t3hmun <git@t3hmun.com>
2018-02-28 21:49:23 +0000

    A very sinple service install bat

---
+++ ServiceWrapper/InstallService.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ^|==================================^|
+echo ^| Simple service installer script. ^|
+echo ^|==================================^|
+echo:
+echo  This script must be run as admin.
+echo  Press the windows key, type in cmd and then press Ctrl-Shift-Enter.
+echo  That will open an command prompt with admin.
+echo:
+echo  Also make sure you have the right name and location for the service...
+echo:
+echo /---------------------------------\
+echo ^|         now, BEHOLD ...         ^|
+echo \_________________________________/
+echo on
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe C:\Services\t3h.ServiceWrapper.exe
